15837822f573f1aa9ca08a257190b29485fb3b97
Agustin Fasano <agusfasano1@gmail.com>
2022-03-19 00:57:45 -0300

    version inicial

---
+++ Desafíos Genéricos/DatosVariables.js
@@ -0,0 +1,25 @@
+//definiendo valores
+let nombre = 'pepe'
+let edad = 25
+let precio = 99.90
+let seriesFavoritas = ['Dark', 'Mr Robot', 'Castlevania']
+let peliculasFavoritas = [
+        {
+            nombre: "Hombre en Llamas",
+            añoEstreno: 2004,
+            protagonistas: ['Denzel Washington', 'Dakota Fanning', 'Christopher Walken']
+
+        },
+        {
+            nombre: 'Interstellar',
+            añoEstreno: 2014,
+            protagonistas: ['Matthew McConaughe', 'Anne Hattaway', 'Michael Cane']
+        }
+]
+//mostrando valores
+console.log(nombre)
+console.log(edad)
+console.log(precio)
+console.log(seriesFavoritas[0, 1, 2])
+console.log(peliculasFavoritas[0])
+console.log(peliculasFavoritas[1])
