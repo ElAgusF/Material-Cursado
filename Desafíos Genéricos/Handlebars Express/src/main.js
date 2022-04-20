15220b07ebd15a6d112976c69145dcd61735329f
Agus Fasano <agusfasano1@gmail.com>
2022-04-20 20:49:22 -0300

    handlebars + express

---
+++ Desafíos Genéricos/Handlebars Express/src/main.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+const hbsConfig = {
+    extname: "hbs",
+    defaultLayout: 'index.hbs' 
+}
+
+app.engine('hbs', exphbs.engine(hbsConfig))
+
+app.set('engine', 'hbs')
+app.set('views', './views')
+
+app.get('/', (req, res) => {
+    res.render('datos.hbs', {
+        nombre:'Agustin',
+        apellido:'Fasano',
+        edad:'23',
+        email:'agus@fasano',
+        telefono:'7777777' 
+    })
+})
+
+app.listen(8080)
